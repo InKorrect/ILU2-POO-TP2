1be621c0a6fab82175b3eb86fe74ade65630ac12
gabriel.plique@gmail.com <gabriel.plique@gmail.com>
2023-03-13 08:10:04 +0100

    FinTP3

--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -13,6 +13,25 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO a  completer
+		String produit,marchandProduit,resultat;
+		int numeroMarchand, nbProduit;
+		boolean possedeProduit;
+		System.out.println("Quel produit voulez vous acheter ?");
+		produit = scan.next();
+		marchandProduit = controlAcheterProduit.AfficherEtalProduit(produit);
+		possedeProduit = marchandProduit.length() == 0;
+		if (possedeProduit) {
+			System.out.println("Desole mais personne ne vend ce produit au marche.");
+		} else {
+			String nomVendeur;
+			numeroMarchand = Clavier
+					.entrerEntier("Chez quel commercant voulez vous acheter des " + produit + " ?\n" + marchandProduit);
+			nomVendeur = controlAcheterProduit.nomVendeur(produit, numeroMarchand);
+			System.out.println(nomAcheteur + " se deplace jusqu a l etal du vendeur " + nomVendeur);
+			nbProduit = Clavier.entrerEntier("Bonjour " + nomAcheteur + "\nCombien de " + produit + " voulez vous ?\n");
+			resultat = controlAcheterProduit.AcheterProduit(produit, numeroMarchand,nbProduit,nomAcheteur);
+			System.out.println(resultat);
+		}
+
 	}
 }
